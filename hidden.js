79d195c8507b333cd32104f261796d4388fab88a
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2021-07-14 14:10:39 -0400

    call works with button!

--- hidden.js
+++ hidden.js
@@ -19,6 +19,69 @@ function handleSubmit (event) {
     event.preventDefault();
     const userInput = document.getElementById('breed');
     breed = userInput.value;
+
+    //clear before append, zip code and style
+    //every time handlesubmit is called, clear the results
+
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+        method: 'POST',
+        body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then(function (resp) {
+    
+        // Return the response as JSON
+        return resp.json();
+    
+    }).then(function (data) {
+    
+        // Log the API data
+        console.log('token', data);
+    
+        // Return a second API call
+        // This one uses the token we received for authentication
+        return fetch('https://api.petfinder.com/v2/animals?breed=' + breed + '&status=' + status, {
+            headers: {
+                'Authorization': data.token_type + ' ' + data.access_token,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+    
+    }).then(function (resp) {
+    
+        // Return the API response as JSON
+        return resp.json();
+    
+    }).then(function (data) {
+    
+        // Log the pet data
+        console.log('pets', data);
+    
+    //start here for print to HTML 
+    let names = data.animals; 
+        return names.map(function(pet) {
+            let li = createNode('li');
+            let img = createNode('img');
+            let span = createNode('span');
+            if (pet.primary_photo_cropped != null) { 
+                img.src = pet.primary_photo_cropped.small}
+            span.innerHTML = `${pet.name}`;
+            append(li, img);
+            append(li, span);
+            append(ul, li);
+        })
+    
+    })
+    
+    .catch(function (err) {
+    
+        // Log any errors
+        console.log('something went wrong', err);
+    
+    });
+
+
 }
 
 submit.addEventListener('click', handleSubmit);
@@ -29,60 +92,3 @@ submit.addEventListener('click', handleSubmit);
 
 // Call the API to get the OAuth token
 // POST request: we need the API to generate a new token for us
-fetch('https://api.petfinder.com/v2/oauth2/token', {
-	method: 'POST',
-	body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
-	headers: {
-		'Content-Type': 'application/x-www-form-urlencoded'
-	}
-}).then(function (resp) {
-
-	// Return the response as JSON
-	return resp.json();
-
-}).then(function (data) {
-
-	// Log the API data
-	console.log('token', data);
-
-	// Return a second API call
-	// This one uses the token we received for authentication
-	return fetch('https://api.petfinder.com/v2/animals?breed=' + breed + '&status=' + status, {
-		headers: {
-			'Authorization': data.token_type + ' ' + data.access_token,
-			'Content-Type': 'application/x-www-form-urlencoded'
-		}
-	});
-
-}).then(function (resp) {
-
-	// Return the API response as JSON
-	return resp.json();
-
-}).then(function (data) {
-
-	// Log the pet data
-	console.log('pets', data);
-
-//start here for print to HTML 
-let names = data.animals; 
-	return names.map(function(pet) {
-		let li = createNode('li');
-		let img = createNode('img');
-		let span = createNode('span');
-		if (pet.primary_photo_cropped != null) { 
-			img.src = pet.primary_photo_cropped.small}
-		span.innerHTML = `${pet.name}`;
-		append(li, img);
-		append(li, span);
-		append(ul, li);
-	})
-
-})
-
-.catch(function (err) {
-
-	// Log any errors
-	console.log('something went wrong', err);
-
-});
